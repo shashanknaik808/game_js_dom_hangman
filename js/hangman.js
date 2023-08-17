0a29afbd8586c5e6de5d9d32e2836cdcc40f083f
Shashank Naik <shashanknaik808@gmail.com>
2023-08-17 12:03:11 +0530

    Checking if the Game was won or lost

--- js/hangman.js
+++ js/hangman.js
@@ -40,30 +40,45 @@ function generateButtons() {
     document.getElementById('keyboard').innerHTML = buttonsHTML;
 }
 
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+    alert(answer);
+
+    if (answer.indexOf(chosenLetter) >= 0) {
+        guessedWord();
+        checkIfGameWon();
+    }
+    else if (answer.indexOf(chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes();
+        checkIfGameLost();
+    }
+}
+
+function checkIfGameWon() {
+    if (wordStatus === answer) {
+        document.getElementById('keyboard').innerHTML = 'You Won!!!';
+    }
+}
+
+function checkIfGameLost() {
+    if (mistakes === maxWrong) {
+        document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+    }
+}
+
+
 function guessedWord() {
     wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
 
     document.getElementById('wordSpotlight').innerHTML = wordStatus;
 }
 
-function handleGuess(chosenLetter) {
-    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
-    document.getElementById(chosenLetter).setAttribute('disabled', true);
-
-    alert(answer);
-  
-    if (answer.indexOf(chosenLetter) >= 0) {
-      guessedWord();
-    }
-    else if (answer.indexOf(chosenLetter) === -1) {
-        mistakes++;
-        updateMistakes();
-    }
-}
-
 function updateMistakes() {
     document.getElementById('mistakes').innerHTML = mistakes;
-  }
+}
 
 document.getElementById('maxWrong').innerHTML = maxWrong;
 
