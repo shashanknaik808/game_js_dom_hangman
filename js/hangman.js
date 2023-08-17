4429963b400e2e167958cef3eeb3fa0b7c5e4c5c
Shashank Naik <shashanknaik808@gmail.com>
2023-08-17 10:57:30 +0530

    Updating Wrong Guesses

--- js/hangman.js
+++ js/hangman.js
@@ -24,3 +24,24 @@ let wordStatus = null;
 function randomWord() {
     answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
 }
+
+function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+        `
+        <button
+          class="btn btn-lg btn-primary m-2"
+          id='` + letter + `'
+          onClick="handleGuess('` + letter + `')"
+        >
+          ` + letter + `
+        </button>
+      `).join('');
+
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+
+
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+randomWord();
+generateButtons();
